1a214410e4a15c2d31d51e370dfc04004c8085fd
nqh00 <nqhb00@gmail.com>
2021-02-23 16:01:58 +0700

    Handling search result, input exception.

--- download.sh
+++ download.sh
@@ -30,28 +30,41 @@ if ! [[ -x "$(command -v aria2c)" ]]; then
 	exit 1;
 fi
 
-# Create temporary directory and temporary text file
-path="$(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")"
-if [ ! -d  $path/__temp__ ]; then
-	mkdir $path/__temp__
-	touch $path/__temp__/temp.txt
-	attrib +h $path/__temp__
-	attrib +h $path/__temp__/temp.txt
-elif [[ ! -d $path/__temp__/temp.txt ]]; then
-	touch $path/__temp__/temp.txt # this file for not raising `cat` exception: no file in directory
-	attrib +h $path/__temp__/temp.txt
-fi
+path="$PWD/__temp__"
 
-# python execution
-run_python() {
-	echo "Search for your anime:"
-	read anime && [[ "$anime" != "" ]] || exit 1
-	echo
-	$python3x "$path/twist.py" "$anime"
+# Create temporary directory and temporary text file
+check_directory () {
+	if [ ! -d  $path ]; then
+		mkdir $path
+		touch $path/temp.txt
+		attrib +h $path
+		attrib +h $path/temp.txt
+	elif [[ ! -d $path/temp.txt ]]; then
+		touch $path/temp.txt # this file for not raising `cat` exception: no file in directory
+		attrib +h $path/temp.txt
+	fi
+}
+
+# Check total url in all text file
+check_total_url () {
+	if [[ "$1" -eq "0" ]]; then
+		run_python
+		if [[ "$found" -eq "1" ]]; then
+			read -p "Press enter to exit and try another keyword."
+			exit 1;
+		fi
+		confirm="yes"
+	elif [[ "$1" -eq "1" ]]; then
+		echo "There are only 1 URL in queue waiting for download."
+		check_state
+	else
+		echo "There are total $1 episodes in queue waiting for download."
+		check_state
+	fi
 }
 
 # Check downloading state
-state_check() {
+check_state () {
 	read -p "Do you want to keep downloading [yes] or renew [no]: " confirm && [[ "$confirm" == [yY] \
 																	 	|| "$confirm" == [yY][eE][sS] \
 																	 	|| "$confirm" == [nN] \
@@ -59,53 +72,45 @@ state_check() {
 														|| exit 1 # Receive only 4 input yes, no, y, n case insensitive
 }
 
+# python execution
+run_python () {
+	echo "Search for your anime:"
+	read -e anime && [[ "$anime" != "" ]] || exit 1
+	echo
+	stty -echo # Disable input
+	python "$PWD/twist.py" "$anime"; found=$(echo $?) # store sys.exit() value to $found, found = 1 is no found
+	stty echo # Re-enable input
+}
+
 # Download all available URL
-download() {
+download () {
 	# Space delimiter
 	IFS=' '
-	for file in $path/__temp__/*.txt; do
+	for file in $path/*.txt; do
 		while read -r _ep _url; do
 			name=$(echo $file | cut -d'/' -f7 | cut -d'.' -f1) # Get the 7th & 1st element of the delimiter array
 			url=${_url%$'\r'} # Strip the `\r` from each line
 			echo "Downloading $name"
 			aria2c "$url" \
-				-o "$name Episode $_ep" \
-				--dir "$path/$name" \
+				-o "$name Episode $_ep.mp4" \
+				--dir "$PWD/$name" \
 				--header="Referer: https://twist.moe/" \
-				--header="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
+				--header="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36" \
+				--always-resume
 		done < "$file"
 	done
-	read -p "Press enter to continue."
+	read -p "Press enter to exit."
+	exit 1;
 }
 
-# Return total lines in all text file combine
-total=$(cat $path/__temp__/*.txt | wc -l | xargs)
-if [[ "$total" -eq "0" ]]; then
-	run_python
-elif [[ "$total" -eq "1" ]]; then
-	echo "There are only 1 URL in queue waiting for download."
-	state_check
-else
-	echo "There are total $total episodes in queue waiting for download."
-	state_check
-fi
-echo
+check_directory
+
+check_total_url "$(cat $path/*.txt | wc -l | xargs)"
 
 if [[ "$confirm" == [yY] || "$confirm" == [yY][eE][sS] ]]; then
 	download
-elif [[ "$confirm" == [nN] || "$confirm" == [nN][oO] ]]; then
-	rm $path/__temp__/*.txt # Clean up
-	run_python
-	echo
-	new_total=$(cat $path/__temp__/*.txt | wc -l | xargs)
-	if [[ "$new_total" -eq "1" ]]; then
-		echo "There are only 1 URL in queue waiting for download."
-		download
-	else
-		echo "There are total $new_total episodes in queue waiting for download."
-		download
-	fi
 else
-	read -p "Press enter to exit."
-	exit 1;
+	rm $path/*.txt # Clean up
+	check_directory
+	check_total_url "$(cat $path/*.txt | wc -l | xargs)"
 fi
